92a112641c21ed496c03a302a151c59184ca3f84
bykplx1 <byk.plai@gmail.com>
2025-02-16 20:07:48 +0700

    Initial project: Task Tracker CLI

---
+++ task-tracker-cli/src/test/java/com/github/bykplx1/tasktracker/task_tracker_cli/TaskTrackerCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bykplx1.tasktracker.task_tracker_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
